08014ac91d95cbf38ad483531acf787745831737
Guillian Prince <guillianprince@R2Hs-MacBook-Pro.local>
2022-01-24 15:07:12 -0500

    added all pages and instructions

---
+++ JavaScript/easy.js
@@ -0,0 +1,9 @@
+//EASY: Write a function that would allow you to do this:
+
+// var run = exercise('running');
+
+// console.log(run()); // prints "Today's exercise: running"
+
+// var swim = exercise('swimming');
+
+// console.log(swim()); // prints "Today's exercise: swimming"
